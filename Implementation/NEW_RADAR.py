bbf2e83aea16d57d57116e8bac395e4bd920dcca
99003713 <78997716+99003713@users.noreply.github.com>
2021-03-10 09:25:20 +0530

    Update NEW_RADAR.py

--- Implementation/NEW_RADAR.py
+++ Implementation/NEW_RADAR.py
@@ -1,1 +1,10 @@
+''' this module will be used for plotting radar chart '''
+from cosolidated_data import *
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
 
+my_data = data_for_graph()
+first_ps = list(my_data.keys())[0]
+topper = top_performer()
+bottom = bottom_performer()
