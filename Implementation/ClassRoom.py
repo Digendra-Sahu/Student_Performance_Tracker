69350b4d34f5dfa8c248fcd087c0de5b343d16db
PS99003709 <digendrasahu11899@hotmail.com>
2021-03-10 09:14:29 +0530

    sum_of_row

--- Implementation/ClassRoom.py
+++ Implementation/ClassRoom.py
@@ -44,3 +44,35 @@ def avg():
     avg_list.append(post_test_avg_list)
 
     return avg_list
+
+
+# Sum of rows for calculating the average marks of the students
+def sum_of_row():
+    res = []
+    post_survey_sum = 0
+    post_test_sum = 0
+    pre_test_sum = 0
+    pre_survey_sum = 0
+    post_survey_row_sum = []
+    pre_survey_row_sum = []
+    post_test_row_sum = []
+    pre_test_row_sum = []
+    for i in range(0, 10):
+        for j in range(4, 10):
+            pre_survey_sum += pre_survey.iloc[i, j]
+            pre_test_sum += pre_test.iloc[i, j]
+            post_survey_sum += post_survey.iloc[i, j]
+            post_test_sum += post_test.iloc[i, j]
+        post_survey_row_sum.append(post_survey_sum)
+        pre_survey_row_sum.append(pre_survey_sum)
+        post_test_row_sum.append(post_test_sum)
+        pre_test_row_sum.append(pre_test_sum)
+        post_survey_sum = 0
+        pre_test_sum = 0
+        pre_survey_sum = 0
+        post_test_sum = 0
+    res.append(pre_survey_row_sum)
+    res.append(pre_test_row_sum)
+    res.append(post_survey_row_sum)
+    res.append(post_test_row_sum)
+    return res
