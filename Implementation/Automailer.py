5953ffc8875beeb3664dfc46bb39c2e7f91ef62a
99003616 <78997665+99003616@users.noreply.github.com>
2021-03-10 10:04:50 +0530

    Attachment code for automailers

--- Implementation/Automailer.py
+++ Implementation/Automailer.py
@@ -137,3 +137,18 @@ def send_mail_student():
         sender_email = 'learningcorporate7@gmail.com'
         sender_ePass = '99003708'
         receiver_email = list1[i]
+                print(receiver_email)
+        message = MIMEMultipart()
+        message['From'] = sender_email
+        message['To'] = receiver_email
+        message['Subject'] = f'Your Test Score :- {psnumber[i]}'
+        mail_text = f'''Hello {psnumber[i]},\n\nThis is to inform you about your test result. Attachments file is above.\n\nThank You\n\nThis is an Auto Generated Mail, Please do not reply to this.'''
+        message.attach(MIMEText(mail_text, 'plain'))
+        with open(image1, "rb") as attachment:
+            part = MIMEBase("application", "octet-stream")
+            part.set_payload(attachment.read())
+
+        encoders.encode_base64(part)
+        part.add_header('Content-Disposition',
+                        "attachment; filename= %s" % image1)
+        message.attach(part)
