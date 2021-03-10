42f414947e504184633a542ed52ae90148254e6b
99003655 <78997751+99003655@users.noreply.github.com>
2021-03-10 09:26:06 +0530

    Create Automailer.py
    Import libraries for automailer

---
+++ Implementation/Automailer.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from smtplib import SMTP
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+from pretty_html_table import build_table
+from email import encoders
+from NEW_RADAR import *
+
